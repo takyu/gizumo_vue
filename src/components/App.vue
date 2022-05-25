911e8908aa6b4bbe5a46d9ed42631529401507af
takyu <takyu613.study@gmail.com>
2022-05-25 17:32:45 +0900

    :construction: components

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,16 +2,21 @@
   <div>
     <h1>{{ message }}</h1>
     <hr />
+    <child-component></child-component>
   </div>
 </template>
 
 <script>
+import ChildComponent from 'Components/ChildComponent';
 export default {
   //es6のメソッド記法
   data() {
     return {
       message: 'Hello Vue',
     };
+  },
+  components: {
+    ChildComponent,
   },
 };
 </script>
