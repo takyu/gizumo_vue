6a44adf50acb061b3417469d89e887510f74b71a
takyu <takyu613.study@gmail.com>
2022-05-26 14:05:37 +0900

    :sparkles: directive

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,8 +1,37 @@
 <template>
   <div>
-    <h1>{{ message }}</h1>
+    <!-- <h1>{{ message }}</h1> -->
+    <!-- <h1 v-html="message" v-bind:class="{ 'is-green': isGreen }"></h1> -->
+    <!-- <h1 v-html="message" v-bind:class="classObject"></h1> -->
+    <h1 v-html="message" :class="classObject"></h1>
     <hr />
-    <child-component></child-component>
+    <child-component v-show="isShow"></child-component>
+    <hr />
+    <!-- <p v-if="id === 1">1</p>
+    <p v-else-if="id === 2">2</p>
+    <p v-else>other</p> -->
+    <template v-if="id === 1">
+      <p>1-1</p>
+      <p>1-2</p>
+      <p>1-3</p>
+    </template>
+    <template v-else-if="id === 2">
+      <p>2-1</p>
+      <p>2-2</p>
+      <p>2-3</p>
+    </template>
+    <hr />
+    <template v-for="item in items">
+      <li :key="item.id">{{ item.id }} - {{ item.title }}</li>
+    </template>
+    <hr />
+    <template v-for="item in items">
+      <child-component :key="item.id"></child-component>
+    </template>
+    <hr />
+    <!-- <button v-on:click="count++">Add to Count</button> -->
+    <button @click="count++">Add to Count</button>
+    <p>{{ count }}回クリックしました。</p>
   </div>
 </template>
 
@@ -13,6 +42,27 @@ export default {
   data() {
     return {
       message: 'Hello Vue',
+      isGreen: true,
+      id: 2,
+      classObject: {
+        'is-green': true,
+      },
+      isShow: true,
+      items: [
+        {
+          id: this.$uuid.v4(),
+          title: '1番目のリスト',
+        },
+        {
+          id: this.$uuid.v4(),
+          title: '2番目のリスト',
+        },
+        {
+          id: this.$uuid.v4(),
+          title: '3番目のリスト',
+        },
+      ],
+      count: 0,
     };
   },
   components: {
@@ -22,11 +72,15 @@ export default {
 </script>
 
 <style scoped>
-h1 {
+/* h1 {
   color: green;
-}
+} */
 
 hr {
   margin: 16px 0;
+}
+
+.is-green {
+  color: green;
 }
 </style>
