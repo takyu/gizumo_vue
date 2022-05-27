1dcb736583064fb2944baac5d9a5db4c833d1493
takyu <takyu613.study@gmail.com>
2022-05-27 11:05:32 +0900

    :sparkles: computed

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <h1>-- methods --</h1>
+    <h1>-- computed --</h1>
     <hr />
     <button @click="incrementCount">Add to count</button>
     <p>{{ count }}回クリックされました</p>
+    <hr />
+    <input type="text" v-model="inputText" />
+    <p>computed: {{ getUpperCaseText }}</p>
+    <p>methods: {{ showUpperCaseText() }}</p>
   </div>
 </template>
 
@@ -35,11 +39,24 @@ export default {
         },
       ],
       count: 0,
+      inputText: '',
     };
   },
   methods: {
     incrementCount() {
       this.count++;
+    },
+    showUpperCaseText() {
+      const upperCaseText = this.inputText.toUpperCase();
+      console.log(`method: ${upperCaseText}`);
+      return upperCaseText;
+    },
+  },
+  computed: {
+    getUpperCaseText() {
+      const upperCaseText = this.inputText.toUpperCase();
+      console.log(`computed: ${upperCaseText}`);
+      return upperCaseText;
     },
   },
   components: {
